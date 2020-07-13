f280547cb6e10be2f3ff41af9017ca4fa001b90a
kenny <fedoranisimov2016@yandex.ru>
2020-07-13 13:15:42 +0300

    only message

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
-print("i full of shit")
-n = 10
-w = n + 1
-print(w + 2)
+import vk_api
+import random
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+
+
+def write_msg(user_id, message):
+    random_id = random.getrandbits(64)  # 64 бита
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id':random_id})
+
+
+# API-ключ созданный ранее
+token = "3dd31c959632a99c0c3b01be18bed4446347ed345e77a865f3dd0b46b0a7ede25228cd75f2ba06ec6e5d4"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
