381b6b216a1fbf622efaf1cf0966e5138b082733
kenny <fedoranisimov2016@yandex.ru>
2020-07-13 20:03:22 +0300

    only message, but classes

---
+++ vk_bot.py
@@ -0,0 +1,21 @@
+class VkBot:
+
+    def __init__(self, user_id):
+        print("Создан объект бота!")
+        self._USER_ID = user_id
+        self._USERNAME = self._get_user_name_from_vk_id(user_id)
+
+        self._COMMANDS = ["ПРИВЕТ", "ПОКА"]
+
+    def new_message(self, message):
+
+        # Привет
+        if message.upper() == self._COMMANDS[0]:
+            return f"Привет-привет, {self._USERNAME}!"
+
+        # Пока
+        elif message.upper() == self._COMMANDS[3]:
+            return f"Пока-пока, {self._USERNAME}!"
+
+        else:
+            return "Не понимаю о чем вы.."
